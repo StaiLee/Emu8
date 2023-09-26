f76fac5ca62130c39c0cf9e2d9c54cff39913339
StaiLee <ilyesstaili@gmail.com>
2023-09-26 23:30:20 +0200

    collecte data

--- main.go
+++ main.go
@@ -9,12 +9,22 @@ import (
 
 type Game struct{}
 
+type Chip8 struct {
+	memory  [4096]byte
+	v       [16]byte
+	i       uint16
+	pc      uint16
+	stack   []uint16
+	sp      int
+	display [64 * 32]byte
+}
+
 func (g *Game) Update() error {
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	ebitenutil.DebugPrint(screen, "")
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -22,8 +32,8 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
+	ebiten.SetWindowSize(1600, 900)
+	ebiten.SetWindowTitle("Emu8")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
