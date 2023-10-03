6ee334fbf85f8ec33ea3865ae53f2b7311a3777d
StaiLee <ilyesstaili@gmail.com>
2023-10-03 09:09:26 +0200

    update opcode, emulation brut non structuré,fonctionelle

--- main.go
+++ main.go
@@ -1,298 +1,407 @@
 package main
 
 import (
-	"fmt"
 	"image/color"
+	"io/ioutil"
 	"log"
+	"math"
 	"math/rand"
+	"os"
 	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-const (
-	screenWidth  = 64
-	screenHeight = 32
-)
+const Font = 80
+const Width = 64 * 10
+const height = 32 * 10
 
 type Chip8 struct {
-	memory     [4096]byte
-	v          [16]byte
-	i          uint16
-	pc         uint16
-	stack      [16]uint16
-	sp         int
-	display    [screenWidth * screenHeight]byte
-	key        [16]bool
-	delay      uint8
-	sound      uint8
-	shouldDraw bool
-	opcode     uint16
+	Memory [4096]uint8
+	V      [16]uint8
+	GFX    [64 * 32]uint8
+	Stack  [16]uint16
+	Key    [16]bool
+	Opcode uint16
+	PC     uint16
+	I      uint16
+	Delay  uint8
+	SP     uint16
+	Sound  uint8
 }
 
-var fontSet = []uint8{
-	0xF0, 0x90, 0x90, 0x90, 0xF0, //0
-	0x20, 0x60, 0x20, 0x20, 0x70, //1
-	0xF0, 0x10, 0xF0, 0x80, 0xF0, //2
-	0xF0, 0x10, 0xF0, 0x10, 0xF0, //3
-	0x90, 0x90, 0xF0, 0x10, 0x10, //4
-	0xF0, 0x80, 0xF0, 0x10, 0xF0, //5
-	0xF0, 0x80, 0xF0, 0x90, 0xF0, //6
-	0xF0, 0x10, 0x20, 0x40, 0x40, //7
-	0xF0, 0x90, 0xF0, 0x90, 0xF0, //8
-	0xF0, 0x90, 0xF0, 0x10, 0xF0, //9
-	0xF0, 0x90, 0xF0, 0x90, 0x90, //A
-	0xE0, 0x90, 0xE0, 0x90, 0xE0, //B
-	0xF0, 0x80, 0x80, 0x80, 0xF0, //C
-	0xE0, 0x90, 0x90, 0x90, 0xE0, //D
-	0xF0, 0x80, 0xF0, 0x80, 0xF0, //E
-	0xF0, 0x80, 0xF0, 0x80, 0x80, //F
+var fontSet [Font]uint8 = [Font]uint8{
+	0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+	0x20, 0x60, 0x20, 0x20, 0x70, // 1
+	0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+	0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+	0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+	0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+	0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+	0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+	0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+	0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+	0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+	0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+	0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+	0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+	0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+	0xF0, 0x80, 0xF0, 0x80, 0x80, // F
 }
 
-func (c *Chip8) LoadROM(romPath string) error {
-	romData, err := ebitenutil.OpenFile(romPath)
+func InitiateChip8() Chip8 {
+	var mem [4096]uint8
+	for i := 0; i < 10; i++ {
+		mem[i] = fontSet[i]
+	}
+	return Chip8{
+		PC:     0x200,
+		Memory: mem,
+	}
+}
+
+func (c *Chip8) LoadGUI(filename string) {
+	bytes, err := ioutil.ReadFile(filename)
 	if err != nil {
-		return err
+		log.Fatalf("unable to read file %v", filename)
 	}
-	defer romData.Close()
-
-	n, err := romData.Read(c.memory[0x200:])
-	if err != nil {
-		return err
+	for i, b := range bytes {
+		c.Memory[512+i] = b
 	}
-
-	fmt.Printf("Loaded %d bytes from the ROM file.\n", n)
-
-	return nil
 }
 
-func (c *Chip8) emulateOpcode(opcode uint16) {
-	x := int((opcode & 0x0F00) >> 8)
-	y := int((opcode & 0x00F0) >> 4)
-	kk := byte(opcode & 0x00FF)
-	nnn := opcode & 0x0FFF
-
-	switch opcode & 0xF000 {
-	case 0x00E0:
-		// Efface l'écran
-		for i := range c.display {
-			c.display[i] = 0
-		}
-		c.shouldDraw = true
-		c.pc += 2
-
-	case 0x00EE:
-		// Retour de sous-routine
-		if c.sp > 0 {
-			c.sp--
-			c.pc = c.stack[c.sp]
-		}
-
-	default:
-		fmt.Printf("Opcode non pris en charge: %X\n", opcode)
+func (c *Chip8) Emulation() {
+	c.opCoding()
+	skip := c.emulateOpcode()
+	if skip {
+		return
 	}
+	c.timerRefresh()
+}
 
-	switch opcode & 0xF000 {
-	case 0x1000:
-		// Saute à l'adresse NNN
-		c.pc = nnn
+func (c *Chip8) opCoding() {
+	c.Opcode = uint16(c.Memory[c.PC])<<8 | uint16(c.Memory[c.PC+1])
+}
 
-	case 0x2000:
-		// Appel de sous-routine
-		c.stack[c.sp] = c.pc
-		c.sp++
-		c.pc = nnn
-
-	case 0x3000:
-		// Sauter si VX est égal à KK
-		if c.v[x] == kk {
-			c.pc += 2
-		}
-
-	case 0x4000:
-		// Sauter si VX n'est pas égal à KK
-		if c.v[x] != kk {
-			c.pc += 2
-		}
-
-	case 0x5000:
-		// Sauter si VX est égal à VY
-		if c.v[x] == c.v[y] {
-			c.pc += 2
-		}
-
-	case 0x6000:
-		// Mettre KK dans VX
-		c.v[x] = kk
-
-	case 0x7000:
-		// Ajouter KK à VX
-		c.v[x] += kk
-
-	case 0x8000:
-		switch opcode & 0x000F {
+func (c *Chip8) emulateOpcode() bool {
+	switch c.Opcode & 0xF000 {
+	case 0x0000:
+		switch c.Opcode & 0x000F {
 		case 0x0000:
-			// Mettre VY dans VX
-			c.v[x] = c.v[y]
 
+			c.GFX = [2048]uint8{}
+			c.PC += 2
+		case 0x000E:
+
+			c.SP--
+			c.PC = c.Stack[c.SP]
+			c.PC += 2
+		default:
+			panicUnknownOpcode(c.Opcode)
+		}
+	case 0x1000:
+
+		c.PC = c.Opcode & 0x0FFF
+	case 0x2000:
+
+		c.Stack[c.SP] = c.PC
+		c.SP++
+		c.PC = c.Opcode & 0x0FFF
+	case 0x3000:
+
+		if c.V[(c.Opcode&0x0F00)>>8] == (uint8(c.Opcode) & 0x00FF) {
+			c.PC += 4
+		} else {
+			c.PC += 2
+		}
+	case 0x4000:
+
+		if c.V[(c.Opcode&0x0F00)>>8] != (uint8(c.Opcode) & 0x00FF) {
+			c.PC += 4
+		} else {
+			c.PC += 2
+		}
+	case 0x5000:
+
+		if c.V[(c.Opcode&0x0F00)>>8] != c.V[(uint8(c.Opcode)&0x00F0)>>4] {
+			c.PC += 4
+		} else {
+			c.PC += 2
+		}
+	case 0x6000:
+
+		c.V[(c.Opcode&0x0F00)>>8] = uint8(c.Opcode) & 0x00FF
+		c.PC += 2
+	case 0x7000:
+
+		c.V[(c.Opcode&0x0F00)>>8] += uint8(c.Opcode) & 0x00FF
+		c.PC += 2
+	case 0x8000:
+		switch c.Opcode & 0x000F {
+		case 0x0000:
+
+			c.V[(c.Opcode&0x0F00)>>8] = c.V[(c.Opcode&0x00F0)>>4]
+			c.PC += 2
 		case 0x0001:
-			// VX = VX | VY
-			c.v[x] |= c.v[y]
 
+			c.V[(c.Opcode&0x0F00)>>8] |= c.V[(c.Opcode&0x00F0)>>4]
+			c.PC += 2
 		case 0x0002:
-			// VX = VX & VY
-			c.v[x] &= c.v[y]
 
+			c.V[(c.Opcode&0x0F00)>>8] &= c.V[(c.Opcode&0x00F0)>>4]
+			c.PC += 2
 		case 0x0003:
-			// VX = VX ^ VY
-			c.v[x] ^= c.v[y]
 
+			c.V[(c.Opcode&0x0F00)>>8] ^= c.V[(c.Opcode&0x00F0)>>4]
+			c.PC += 2
 		case 0x0004:
-			// Addition avec retenue
-			if int(c.v[x])+int(c.v[y]) > 255 {
-				c.v[0xF] = 1
-			} else {
-				c.v[0xF] = 0
-			}
-			c.v[x] += c.v[y]
 
-		case 0x0005:
-			// Soustraction avec retenue
-			if c.v[x] > c.v[y] {
-				c.v[0xF] = 1
+			if c.V[(c.Opcode&0x00F0)>>4] > (0xFF - c.V[(c.Opcode&0x0F00)>>8]) {
+				c.V[0xF] = 1
 			} else {
-				c.v[0xF] = 0
+				c.V[0xF] = 0
 			}
-			c.v[x] -= c.v[y]
+			c.V[(c.Opcode&0x0F00)>>8] += c.V[(c.Opcode&0x00F0)>>4]
+			c.PC += 2
+		case 0x0005:
+
+			if c.V[(c.Opcode&0x00F0)>>4] > c.V[(c.Opcode&0x0F00)>>8] {
+
+				c.V[0xF] = 0
+			} else {
+
+				c.V[0xF] = 1
+			}
+			c.V[(c.Opcode&0x0F00)>>8] -= c.V[(c.Opcode&0x00F0)>>4]
+			c.PC += 2
 
 		case 0x0006:
-			// Décalage à droite
-			c.v[0xF] = c.v[x] & 0x01
-			c.v[x] >>= 1
 
+			c.V[0xF] = c.V[(c.Opcode&0x0F00)>>8] & 0x1
+			c.V[(c.Opcode&0x0F00)>>8] >>= 1
+			c.PC += 2
 		case 0x0007:
-			// VX = VY - VX
-			if c.v[y] > c.v[x] {
-				c.v[0xF] = 0
-			} else {
-				c.v[0xF] = 1
-			}
-			c.v[x] = c.v[y] - c.v[x]
 
+			if c.V[(c.Opcode&0x0F00)>>8] > c.V[(c.Opcode&0x00F0)>>4] {
+				c.V[0xF] = 0
+			} else {
+				c.V[0xF] = 1
+			}
+			c.V[(c.Opcode&0x0F00)>>8] = c.V[(c.Opcode&0x00F0)>>4] - c.V[(c.Opcode&0x0F00)>>8]
+			c.PC += 2
 		case 0x000E:
-			// Décalage à gauche
-			c.v[0xF] = (c.v[x] >> 7) & 0x01
-			c.v[x] <<= 1
+
+			c.V[0xF] = c.V[(c.Opcode&0x0F00)>>8] >> 7
+			c.V[(c.Opcode&0x0F00)>>8] <<= 1
+			c.PC += 2
+		default:
+			panicUnknownOpcode(c.Opcode)
+		}
+	case 0x9000:
+
+		if c.V[(c.Opcode&0x0F00)>>8] != c.V[(c.Opcode&0x00F0)>>4] {
+			c.PC += 4
+		} else {
+			c.PC += 2
+		}
+	case 0xA000:
+
+		c.I = c.Opcode & 0x0FFF
+		c.PC += 2
+	case 0xB000:
+
+		c.PC = (c.Opcode & 0x0FFF) + uint16(c.V[0])
+	case 0xC000:
+
+		c.V[(c.Opcode&0x0F00)>>8] = randomByte() & uint8(c.Opcode&0x00FF)
+		c.PC += 2
+	case 0xD000:
+
+		x := uint16(c.V[(c.Opcode&0x0F00)>>8])
+		y := uint16(c.V[(c.Opcode&0x00F0)>>4])
+		height := uint16(c.Opcode & 0x000F)
+		var pixel uint16
+
+		c.V[0xF] = 0
+		for yline := uint16(0); yline < height; yline++ {
+			pixel = uint16(c.Memory[c.I+yline])
+			for xline := uint16(0); xline < 8; xline++ {
+				if (pixel & (0x80 >> xline)) != 0 {
+					if c.GFX[x+xline+((y+yline)*64)] == 1 {
+						c.V[0xF] = 1
+					}
+					c.GFX[x+xline+((y+yline)*64)] ^= 1
+				}
+			}
+		}
+
+		c.PC += 2
+
+	case 0xE000:
+		switch c.Opcode & 0x00FF {
+		case 0x009E:
+
+			if c.Key[c.V[(c.Opcode&0x0F00)>>8]] {
+				c.PC += 4
+			} else {
+				c.PC += 2
+			}
+		case 0x00A1:
+
+			if !c.Key[c.V[(c.Opcode&0x0F00)>>8]] {
+				c.PC += 4
+			} else {
+				c.PC += 2
+			}
+		default:
+			panicUnknownOpcode(c.Opcode)
+		}
+	case 0xF000:
+		switch c.Opcode & 0x00FF {
+		case 0x0007:
+
+			c.V[(c.Opcode&0x0F00)>>8] = c.Delay
+			c.PC += 2
+		case 0x000A:
+			keyPress := false
+			for i := uint8(0); i < 16; i++ {
+				if c.Key[i] {
+					c.V[(c.Opcode&0x0F00)>>8] = i
+					keyPress = true
+				}
+			}
+			if !keyPress {
+				return true
+			}
+			c.PC += 2
+		case 0x0015:
+
+			c.Delay = c.V[(c.Opcode&0x0F00)>>8]
+			c.PC += 2
+		case 0x0018:
+
+			c.Sound = c.V[(c.Opcode&0x0F00)>>8]
+			c.PC += 2
+		case 0x001E:
+
+			if c.I+uint16(c.V[(c.Opcode&0x0F00)>>8]) > 0xFFF {
+				c.V[0xF] = 1
+			} else {
+				c.V[0xF] = 0
+			}
+			c.I += uint16(c.V[(c.Opcode&0x0F00)>>8])
+			c.PC += 2
+		case 0x0029:
+
+			c.I = uint16(c.V[(c.Opcode&0x0F00)>>8]) * 0x5
+			c.PC += 2
+		case 0x0033:
+
+			c.Memory[c.I] = c.V[(c.Opcode&0x0F00)>>8] / 100
+			c.Memory[c.I+1] = (c.V[(c.Opcode&0x0F00)>>8] / 10) % 10
+			c.Memory[c.I+2] = (c.V[(c.Opcode&0x0F00)>>8] % 100) % 10
+			c.PC += 2
+		case 0x0055:
+
+			for i := uint16(0); i <= ((c.Opcode & 0x0F00) >> 8); i++ {
+				c.Memory[c.I+i] = c.V[i]
+			}
+			c.I += ((c.Opcode & 0x0F00) >> 8) + 1
+			c.PC += 2
+		case 0x0065:
+
+			for i := uint16(0); i <= ((c.Opcode & 0x0F00) >> 8); i++ {
+				c.V[i] = c.Memory[c.I+i]
+			}
+			c.I += ((c.Opcode & 0x0F00) >> 8) + 1
+			c.PC += 2
 
 		default:
-			fmt.Printf("Opcode non pris en charge: %X\n", opcode)
+			panicUnknownOpcode(c.Opcode)
 		}
-
-	case 0x9000:
-		// Sauter si VX n'est pas égal à VY
-		if c.v[x] != c.v[y] {
-			c.pc += 2
-		}
-
-	case 0xA000:
-		// Mettre NNN dans I
-		c.i = nnn
-
-	case 0xB000:
-		// Sauter à l'adresse NNN + V0
-		c.pc = nnn + uint16(c.v[0])
-
-	case 0xC000:
-		// Mettre un nombre aléatoire ET KK dans VX
-		randomValue := byte(rand.Intn(256))
-		c.v[x] = randomValue & kk
-
-	case 0xD000:
-		// Dessine un sprite à l'écran
-		xPos := int(c.v[x])
-		yPos := int(c.v[y])
-		height := int(opcode & 0x000F)
-		collision := false
-
-		for row := 0; row < height; row++ {
-			pixel := c.memory[c.i+uint16(row)]
-			y := (yPos + row) % screenHeight
-
-			for col := 0; col < 8; col++ {
-				x := (xPos + col) % screenWidth
-
-				if (pixel & (0x80 >> col)) != 0 {
-					if c.display[y*screenWidth+x] == 1 {
-						collision = true
-					}
-					c.display[y*screenWidth+x] ^= 1
-				}
-			}
-		}
-
-		if collision {
-			c.v[0xF] = 1
-		} else {
-			c.v[0xF] = 0
-		}
-
-		c.shouldDraw = true
-		c.pc += 2
-
 	default:
-		fmt.Printf("Opcode non pris en charge: %X\n", opcode)
+		panicUnknownOpcode(c.Opcode)
+	}
+	return false
+}
+
+func panicUnknownOpcode(opcode uint16) {
+	log.Panicf("opcode non reconnu %v", opcode)
+}
+
+func (c *Chip8) timerRefresh() {
+	if c.Delay > 0 {
+		c.Delay--
+	}
+	if c.Sound > 0 {
+		c.Sound--
 	}
 }
 
-func (c *Chip8) Draw(screen *ebiten.Image) {
-	if c.shouldDraw {
-		for y := 0; y < screenHeight; y++ {
-			for x := 0; x < screenWidth; x++ {
-				// Dessinez un pixel blanc si c.display[y*screenWidth+x] vaut 1, sinon noir
-				if c.display[y*screenWidth+x] == 1 {
-					screen.Set(x, y, color.White)
-				} else {
-					screen.Set(x, y, color.Black)
-				}
-			}
-		}
-		c.shouldDraw = false
+func (c *Chip8) input() {
+	c.Key = [16]bool{
+		ebiten.IsKeyPressed(ebiten.KeyX),
+		ebiten.IsKeyPressed(ebiten.Key1),
+		ebiten.IsKeyPressed(ebiten.Key2),
+		ebiten.IsKeyPressed(ebiten.Key3),
+		ebiten.IsKeyPressed(ebiten.KeyQ),
+		ebiten.IsKeyPressed(ebiten.KeyW),
+		ebiten.IsKeyPressed(ebiten.KeyE),
+		ebiten.IsKeyPressed(ebiten.KeyA),
+		ebiten.IsKeyPressed(ebiten.KeyS),
+		ebiten.IsKeyPressed(ebiten.KeyD),
+		ebiten.IsKeyPressed(ebiten.KeyZ),
+		ebiten.IsKeyPressed(ebiten.KeyC),
+		ebiten.IsKeyPressed(ebiten.Key4),
+		ebiten.IsKeyPressed(ebiten.KeyR),
+		ebiten.IsKeyPressed(ebiten.KeyF),
+		ebiten.IsKeyPressed(ebiten.KeyV),
 	}
 }
 
-func (c *Chip8) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return outsideWidth, outsideHeight
+func randomByte() uint8 {
+	rand.Seed(time.Now().UTC().UnixNano())
+	randint := rand.Intn(math.MaxUint8)
+	return uint8(randint)
 }
 
-func (c *Chip8) Update() error {
-	// Lire l'opcode à partir de la mémoire à la position actuelle du PC
-	opcode := uint16(c.memory[c.pc])<<8 | uint16(c.memory[c.pc+1])
-
-	// Appeler emulateOpcode pour émuler l'opcode
-	c.emulateOpcode(opcode)
-
-	// La logique de mise à jour de l'émulateur va ici
-
+func (g *Chip8) Update() error {
+	g.input()
+	g.Emulation()
 	return nil
+}
+
+func (g *Chip8) Draw(screen *ebiten.Image) {
+	for row := 0; row < height; row++ {
+		for col := 0; col < Width; col++ {
+			isOn := g.GFX[((row/10)*64)+(col/10)] == 1
+			var colorToUse color.Color
+			if isOn {
+				colorToUse = color.White
+			} else {
+				colorToUse = color.Black
+			}
+			screen.Set(col, row, colorToUse)
+		}
+	}
+}
+
+func (g *Chip8) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return Width, height
 }
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-
-	ebiten.SetWindowSize(screenWidth*10, screenHeight*10)
-	ebiten.SetWindowTitle("CHIP-8 Emulator")
-
-	chip8 := &Chip8{}
-
-	// Chargez la ROM que vous souhaitez exécuter
-	romPath := "roms8/filter.ch8"
-	if err := chip8.LoadROM(romPath); err != nil {
-		log.Fatal(err)
+	if len(os.Args) < 2 {
+		log.Fatal("roms8/file.ch8")
 	}
 
-	// Démarrez Ebiten
-	if err := ebiten.RunGame(chip8); err != nil {
+	programName := os.Args[1]
+	chip := InitiateChip8()
+	chip.LoadGUI(programName)
+
+	ebiten.SetWindowSize(Width, height)
+	ebiten.SetWindowTitle(programName)
+	if err := ebiten.RunGame(&chip); err != nil {
 		log.Fatal(err)
 	}
 }
